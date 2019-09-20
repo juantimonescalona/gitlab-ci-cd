a695a850220276b7698c1963ce79b25c6b92b348
Juan Timon Escalona <juan.timon.escalona@gmail.com>
2019-09-20 14:39:55 +0200

    Update migrator.sh

--- example/pipeline-for-generator/migrator.sh
+++ example/pipeline-for-generator/migrator.sh
@@ -1,8 +1,24 @@
+# echo input vars
+echo "Name from copy: $ARCHETYPE_NAME"
+echo "New project name: $PROJECT_NAME"
+echo "Group Owner: $GROUP_OWNER"
+echo "User IC: $USER_IC"
+# $PASSWORD_IC
+
 # clone the template project to local
+# git clone ssh://john@example.com/path/to/my-project.git
+git clone https://$USER_IC:$PASSWORD_IC@$ARCHETYPE_NAME.git
 
-# rename project name 
+# rename project name
+mv $ARCHETYPE_NAME $PROJECT_NAME
 
-# assign to other user the project repo
+# Sharing a project with a group
+# 10 => Guest access
+# 20 => Reporter access
+# 30 => Developer access
+# 40 => Maintainer access
+# 50 => Owner access # Only valid for groups
+GROUP_OWNER
 
 # push to new origin
 
