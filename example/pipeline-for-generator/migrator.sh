0397e68fc2c88b88fc7eb0a96814e8e163e81f93
Juan Timon Escalona <juan.timon.escalona@gmail.com>
2019-09-24 10:32:16 +0200

    Update migrator.sh

--- example/pipeline-for-generator/migrator.sh
+++ example/pipeline-for-generator/migrator.sh
@@ -1,9 +1,11 @@
 # echo input vars
-echo "Name from copy: $ARCHETYPE_NAME"
+echo "Name archetype from copy: $ARCHETYPE_NAME"
 echo "New project name: $PROJECT_NAME"
 echo "Group Owner: $GROUP_OWNER"
-echo "User IC: $USER_IC"
-# $PASSWORD_IC
+echo "User IC: $USER_IC" #ic_user (en variable)
+echo "Server host: $SERVER_HOST"
+# $PASSWORD_IC (en variable)
+# $SERVER_URL (en variable)
 
 # clone the template project to local
 # git clone ssh://john@example.com/path/to/my-project.git
@@ -19,7 +21,9 @@ mv $ARCHETYPE_NAME $PROJECT_NAME
 # 30 => Developer access
 # 40 => Maintainer access
 # 50 => Owner access # Only valid for groups
-GROUP_OWNER
 
-# push to new origin
+# Create project for group and push to new origin
+cd $PROJECT_NAE
+# git push --set-upstream https://gitlab.example.com/namespace/nonexistent-project.git master
+git push --set-upstream $SERVER_URL/$GROUP_OWNER/$PROJECT_NAME.git master
 
