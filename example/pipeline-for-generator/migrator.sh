e7d566c59e0cf5a6c5b1420d5ba1650b2e20b41d
Juan Timon Escalona <juan.timon.escalona@gmail.com>
2019-09-24 14:28:52 +0200

    Update migrator.sh

--- example/pipeline-for-generator/migrator.sh
+++ example/pipeline-for-generator/migrator.sh
@@ -9,17 +9,20 @@ echo "HTTP/HTTPS Protocol: $PROTOCOL" #http (en variabe)
 
 # clone the template project to local
 cd ..
-echo "git clone from $SERVER_HOST the archetype project $ARCHETYPE_NAME of user $USER_IC"
+rm -rf tmp_clone/
+mkdir tmp_clone
+cd tmp_clone
 echo "git clone $PROTOCOL://$USER_IC:$PASSWORD_IC@$SERVER_HOST/$USER_IC/$ARCHETYPE_NAME.git"
-# git clone http://ic_user:*****@192.168.1.53:9080/ic_user/template1.git
 git clone $PROTOCOL://$USER_IC:$PASSWORD_IC@$SERVER_HOST/$USER_IC/$ARCHETYPE_NAME.git
 
 # rename project name
 echo "Rename project $ARCHETYPE_NAME to $PROJECT_NAME"
-echo "PWD path is $PWD"
-mv $ARCHETYPE_NAME $PROJECT_NAME
+mkdir $PROJECT_NAME
+cp -r ./$ARCHETYPE_NAME/* ./$PROJECT_NAME && rm -rf ./$ARCHETYPE_NAME/
 
 # Create project for group and push to new origin
 cd $PROJECT_NAME
-# git push --set-upstream https://gitlab.example.com/namespace/nonexistent-project.git master
-git push --set-upstream $SERVER_URL/$GROUP_OWNER/$PROJECT_NAME.git master
+# git remote add upstream
+git init
+git remote add origin $PROTOCOL://$USER_IC:$PASSWORD_IC@$SERVER_HOST/$GROUP_OWNER/$PROJECT_NAME
+git push --set-upstream origin --all
